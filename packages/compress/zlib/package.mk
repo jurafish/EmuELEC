7aa334a095fbc91482ea4c39ccb68e6943bd1de3
jurafish <jurafish@163.com>
2022-10-27 01:52:24 +0800

    Update package.mk

--- packages/compress/zlib/package.mk
+++ packages/compress/zlib/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="zlib"
-PKG_VERSION="1.2.12"
-PKG_SHA256="7db46b8d7726232a621befaab4a1c870f00a90805511c0e0090441dac57def18"
+PKG_VERSION="1.2.13"
+PKG_SHA256="b3a24de97a8fdbc835b9833169501030b8977031bcb54b3b3ac13740f846ab30"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.zlib.net"
 PKG_URL="http://zlib.net/${PKG_NAME}-${PKG_VERSION}.tar.xz"
